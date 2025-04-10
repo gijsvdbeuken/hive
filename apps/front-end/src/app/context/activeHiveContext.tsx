13cc70e7978ad3ea54da75841211b8d1f2e4e2ac
gijsvandenbeuken <gijs.vandenbeuken@gmail.com>
2025-04-10 13:10:28 +0200

    fixed some eslint bugs

--- apps/front-end/src/app/context/activeHiveContext.tsx
+++ apps/front-end/src/app/context/activeHiveContext.tsx
@@ -1,14 +1,25 @@
 'use client';
-import { createContext, useState, useContext } from 'react';
+import { createContext, useState, useContext, ReactNode } from 'react';
 
-const activeHiveContext = createContext<any>(undefined);
+interface ActiveHiveContextType {
+  activeHive: string;
+  setActiveHive: (value: string) => void;
+}
 
-export function AppWrapper({ children }: { children: React.ReactNode }) {
-  let [activeHive, setActiveHive] = useState('Allround Hive');
+const activeHiveContext = createContext<ActiveHiveContextType | undefined>(undefined);
+
+export function AppWrapper({ children }: { children: ReactNode }) {
+  const [activeHive, setActiveHive] = useState('Allround Hive');
 
   return <activeHiveContext.Provider value={{ activeHive, setActiveHive }}>{children}</activeHiveContext.Provider>;
 }
 
 export function useAppContext() {
-  return useContext(activeHiveContext);
+  const context = useContext(activeHiveContext);
+
+  if (!context) {
+    throw new Error('useAppContext must be used within an AppWrapper');
+  }
+
+  return context;
 }
--- apps/front-end/src/app/hives/page.tsx
+++ apps/front-end/src/app/hives/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import HiveBlock from './components/HiveBlock';
 import { useAppContext } from '../context/activeHiveContext';
 
