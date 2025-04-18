c5f38137c1e09058766352ebfa35b4c2c329cfc6
gijsvandenbeuken <gijs.vandenbeuken@gmail.com>
2025-04-18 11:22:28 +0200

    setup global server execution

--- apps/api-gateway/src/server.js
+++ apps/api-gateway/src/server.js
@@ -14,7 +14,7 @@ app.use(
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`API Gateway is running on http://localhost:${PORT}`);
+  console.log(`api gateway is running on http://localhost:${PORT}`);
 });
 
 setInterval(() => {}, 60000);
