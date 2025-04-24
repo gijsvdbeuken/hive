eec347f83dbe040721039128305ce1b731c1ed59
gijsvandenbeuken <gijs.vandenbeuken@gmail.com>
2025-04-24 10:48:21 +0200

    setting up initial auth0 structure

--- apps/front-end/src/app/page.tsx
+++ apps/front-end/src/app/page.tsx
@@ -1,10 +1,13 @@
+/*
 'use client';
+import { auth0 } from '@/lib/auth0';
 import Question from './components/home/Question';
 import Answer from './components/home/Answer';
 import Questionbar from './components/home/Questionbar';
 import { useActiveChatContext } from './context/activeChatContext';
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth0.getSession();
   const { messages, addMessage, updateMessages } = useActiveChatContext();
 
   async function handleSubmit(message: string) {
@@ -25,6 +28,19 @@ export default function Home() {
     }
   }
 
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">
+          <button>Sign up</button>
+        </a>
+        <a href="/auth/login">
+          <button>Log in</button>
+        </a>
+      </main>
+    );
+  }
+
   return (
     <div className="flex h-[100%] flex-col items-center justify-end font-albert">
       <div className="flex w-[800px] flex-col items-center justify-end font-albert">
@@ -36,6 +52,43 @@ export default function Home() {
         ))}
         <Questionbar onSubmit={handleSubmit} />
       </div>
+      <a href="/auth/logout">
+        <button>Log out</button>
+      </a>
     </div>
+  );
+}
+*/
+import { auth0 } from '@/lib/auth0';
+import './globals.css';
+
+export default async function Home() {
+  // Fetch the user session
+  const session = await auth0.getSession();
+
+  // If no session, show sign-up and login buttons
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">
+          <button>Sign up</button>
+        </a>
+        <a href="/auth/login">
+          <button>Log in</button>
+        </a>
+      </main>
+    );
+  }
+
+  // If session exists, show a welcome message and logout button
+  return (
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+      <p>
+        <a href="/auth/logout">
+          <button>Log out</button>
+        </a>
+      </p>
+    </main>
   );
 }
