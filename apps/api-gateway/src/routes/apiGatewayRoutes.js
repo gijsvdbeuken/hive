19deea549053ff0fdd927997d7a0fe452f0ea4b6
gijsvandenbeuken <gijs.vandenbeuken@gmail.com>
2025-05-07 11:58:12 +0200

    minor adjustments

--- apps/api-gateway/src/routes/apiGatewayRoutes.js
+++ apps/api-gateway/src/routes/apiGatewayRoutes.js
@@ -17,37 +17,6 @@ const ROUTING_MAP = {
 
 const router = express.Router();
 
-/*
-const MICROSERVICE_URL = `http://localhost:${process.env.SERVICE_LLM_RESPONSES}/api/chat`;
-
-
-async function forwardRequest(path, req, res) {
-  try {
-    const response = await fetch(`${MICROSERVICE_URL}${path}`, {
-      method: req.method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(req.body),
-    });
-    console.log('Message has been sent through API gateway...');
-
-    if (!response.ok) {
-      throw new Error(`Microservice responded with ${response.status}`);
-    }
-
-    const data = await response.json();
-    res.json(data);
-  } catch (error) {
-    console.error('Forwarding error:', error);
-    res.status(500).json({
-      error: 'Failed to forward request to microservice',
-      details: error.message,
-    });
-  }
-}
-*/
-
 async function forwardRequest(req, res) {
   const path = req.path;
   const servicePrefix = Object.keys(ROUTING_MAP).find((prefix) => path.startsWith(prefix));
@@ -82,30 +51,6 @@ async function forwardRequest(req, res) {
     });
   }
 }
-
-/*
-router.post('/test', async (req, res) => {
-  await forwardRequest('/test', req, res);
-});
-
-router.post('/openai', async (req, res) => {
-  await forwardRequest('/openai', req, res);
-});
-
-router.post('/anthropic', async (req, res) => {
-  await forwardRequest('/anthropic', req, res);
-});
-
-router.post('/batches/save-active-hive', async (req, res) => {
-  const { activeBatch } = req.body;
-
-  if (!activeBatch || typeof activeBatch !== 'string') {
-    return res.status(400).json({ error: 'Invalid or missing "activeBatch" string' });
-  }
-  console.log(`Received active hive: ${activeBatch}`);
-  return res.status(200).json({ status: 'ok' });
-});
-*/
 
 router.use(async (req, res) => {
   await forwardRequest(req, res);
