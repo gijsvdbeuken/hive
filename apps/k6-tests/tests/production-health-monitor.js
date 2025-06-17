0cdbd88beb4b1ac346cdfa3d3c698548a3e2dbcb
gijsvandenbeuken <gijs.vandenbeuken@gmail.com>
2025-06-17 16:03:38 +0200

    Testing to see wether if I get notified properly on failed monitoring

--- apps/k6-tests/tests/production-health-monitor.js
+++ apps/k6-tests/tests/production-health-monitor.js
@@ -4,7 +4,7 @@ import { check, sleep } from 'k6';
 export const options = {
   stages: [{ duration: '30s', target: 2 }],
   thresholds: {
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(95)<10'],
     http_req_failed: ['rate<0.1'],
     checks: ['rate>0.95'],
   },
